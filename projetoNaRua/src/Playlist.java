4fefd895218b3aeb03e2dfb7a4c7d4e17fb0bb34
Saulo Ferraz <ferrazsaulo1@gmail.com>
2023-04-25 14:03:57 -0300

    Dps termino

--- projetoNaRua/src/Playlist.java
+++ projetoNaRua/src/Playlist.java
@@ -1,3 +1,7 @@
 public class Playlist {
-    
+        
+    private String nome;
+    private String descricao;
+    private Usuario criador;
+    private String data;
 }
--- projetoNaRua/src/Usuario.java
+++ projetoNaRua/src/Usuario.java
@@ -1,3 +1,63 @@
+import java.util.ArrayList;
+
 public class Usuario {
     
+    private String username;
+    private String senha;
+    private String email;
+    private String nomeCompleto;
+    private String tipoUsuario;
+    private int idUsuario;
+
+    public Usuario() {}
+
+    public Usuario(String username, String senha, String email, String nomeCompleto , 
+                    String tipoUsuario, int idUsuario) {
+
+        this.username = username;
+        this.senha = senha;
+        this.email = email;
+        this.nomeCompleto = nomeCompleto;
+        this.tipoUsuario = tipoUsuario;
+        this.idUsuario = idUsuario;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
 }
